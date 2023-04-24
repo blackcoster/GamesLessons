8f4015731da390f339bfb8c14bccb95e28f2d7bf
Polina Krupenina <krupenina.p@mail.ru>
2023-04-24 21:11:57 +0300

    Обработка коллизий

--- Lesson41/1.py
+++ Lesson41/1.py
@@ -14,34 +14,82 @@ WIDTH = 22
 HEIGHT = 32
 COLOR = '#888888'
 
+JUMP_POWER = 10
+GRAVITY = 0.35
+
+
+class Platform(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+        self.image.fill(Color(PLATFORM_COLOR))
+        self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+
+
 class Player(sprite.Sprite):
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         sprite.Sprite.__init__(self)
         self.xvel = 0
         self.startX = x
         self.startY = y
-
-        self.image = Surface((WIDTH,HEIGHT))
+        self.yvel = 0
+        self.onGround = False
+        self.image = Surface((WIDTH, HEIGHT))
         self.image.fill(Color(COLOR))
 
-        self.rect = Rect(x,y,WIDTH,HEIGHT)
+        self.rect = Rect(x, y, WIDTH, HEIGHT)
 
-    def update(self,left,right):
+    def update(self, left, right, up, platform):
+        if up:
+            if self.onGround:
+                self.yvel-=JUMP_POWER
+
         if left:
             self.xvel = -MOVE_SPEED
 
         if right:
             self.xvel = MOVE_SPEED
 
-        if not(left or right):
+        if not (left or right):
             self.xvel = 0
 
-        self.rect.x += self.xvel # сдвигаем координаты игрока на значение смещения
+        if not self.onGround:
+            self.yvel += GRAVITY
+        self.onGround = False
 
-    def draw(self):
-        screen.blit(self.image,(self.rect.x, self.rect.y))
+        self.rect.y += self.yvel
+        self.collide(0, self.yvel, platform)
+        self.rect.x += self.xvel  # сдвигаем координаты игрока на значение смещения
+        self.collide(self.xvel, 0, platform)
+
+    def collide(self, xvel, yvel, platform):
+        for p in platform:
+            if sprite.collide_rect(self, p):
+
+                if xvel > 0:
+                    self.rect.right = p.rect.left
+
+                if xvel < 0:
+                    self.rect.left = p.rect.right
+
+                if yvel > 0:
+                    self.rect.bottom = p.rect.top
+                    self.onGround = True
+                    self.yvel = 0
+
+                if yvel < 0:
+                    self.rect.top = p.rect.bottom
+                    self.yvel = 0
 
 
+hero = Player(55, 55)
+left = False
+right = False
+up = False
+
+entities = pygame.sprite.Group()
+platform = []
+entities.add(hero)
 level = [
     "-------------------------",
     "-                       -",
@@ -63,6 +111,20 @@ level = [
     "-                       -",
     "-                       -",
     "-------------------------"]
+timer = pygame.time.Clock()
+
+x = 0
+y = 0
+for row in level:
+    for col in row:
+        if col == '-':
+            pf = Platform(x, y)
+            entities.add(pf)
+            platform.append(pf)
+
+        x += PLATFORM_WIDTH
+    y += PLATFORM_HEIGHT
+    x = 0
 
 pygame.init()
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -72,27 +134,27 @@ bg = Surface((WIN_WIDTH, WIN_HEIGHT))
 bg.fill(Color(BACKGROUND_COLOR))
 
 while True:
+    timer.tick(60)
     for e in pygame.event.get():
         if e.type == QUIT:
             raise SystemExit
+        if e.type == KEYDOWN and e.key == K_LEFT:
+            left = True
+        if e.type == KEYDOWN and e.key == K_RIGHT:
+            right = True
 
-        screen.blit(bg, (0, 0))  # отрисовка поверхности bg на поверхности screen
+        if e.type == KEYUP and e.key == K_LEFT:
+            left = False
+        if e.type == KEYUP and e.key == K_RIGHT:
+            right = False
 
-        # x = y = 0
-        x = 0
-        y = 0
-        for row in level:
-            for col in row:
-                if col == '-':
-                    pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-                    pf.fill(Color(PLATFORM_COLOR))
-                    screen.blit(pf,(x,y))
+        if e.type == KEYDOWN and e.key == K_UP:
+            up = True
+        if e.type == KEYUP and e.key == K_UP:
+            up = False
 
-                x += PLATFORM_WIDTH
-            y+= PLATFORM_HEIGHT
-            x = 0
+    screen.blit(bg, (0, 0))  # отрисовка поверхности bg на поверхности screen
+    hero.update(left, right, up, platform)
+    entities.draw(screen)
 
-
-
-
-        pygame.display.update()
+    pygame.display.update()
