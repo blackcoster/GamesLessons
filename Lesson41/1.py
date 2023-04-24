6fb56567d117076fadc28e1f37e96cac947cbee0
Polina Krupenina <krupenina.p@mail.ru>
2023-04-24 20:05:36 +0300

    Обработка коллизий

--- Lesson41/1.py
+++ Lesson41/1.py
@@ -1,13 +1,46 @@
 import pygame
 from pygame import *
 
-WIDTH = 800
-HEIGHT = 640
+WIN_WIDTH = 800
+WIN_HEIGHT = 640
 BACKGROUND_COLOR = '#004400'
 
 PLATFORM_WIDTH = 32
 PLATFORM_HEIGHT = 32
 PLATFORM_COLOR = '#FF6262'
+
+MOVE_SPEED = 7
+WIDTH = 22
+HEIGHT = 32
+COLOR = '#888888'
+
+class Player(sprite.Sprite):
+    def __init__(self,x,y):
+        sprite.Sprite.__init__(self)
+        self.xvel = 0
+        self.startX = x
+        self.startY = y
+
+        self.image = Surface((WIDTH,HEIGHT))
+        self.image.fill(Color(COLOR))
+
+        self.rect = Rect(x,y,WIDTH,HEIGHT)
+
+    def update(self,left,right):
+        if left:
+            self.xvel = -MOVE_SPEED
+
+        if right:
+            self.xvel = MOVE_SPEED
+
+        if not(left or right):
+            self.xvel = 0
+
+        self.rect.x += self. # сдвигаем координаты игрока на значение смещения
+
+    def draw(self):
+        screen.blit(self.image,(self.rect.x, self.rect.y))
+
 
 level = [
     "-------------------------",
@@ -32,10 +65,10 @@ level = [
     "-------------------------"]
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption('Платформер')
 
-bg = Surface((WIDTH, HEIGHT))
+bg = Surface((WIN_WIDTH, WIN_HEIGHT))
 bg.fill(Color(BACKGROUND_COLOR))
 
 while True:
@@ -58,6 +91,7 @@ while True:
                 x += PLATFORM_WIDTH
             y+= PLATFORM_HEIGHT
             x = 0
+
 
 
 
